zephyrproject-rtos/zephyr

drivers: clock_control: infineon: Add PSOC4000T family support
Add the PSOC4000T-specific peripheral clock divider handling
needed for the new SoC series.

Signed-off-by: Yurii Lozynskyi <Yurii.Lozynskyi-EE@infineon.com>

drivers/clock_control/clock_control_infineon_fixed_clock.c
@@ -170,7 +170,9 @@ static int fixed_rate_clk_init(const struct device *dev)
 		/* "touch" err to avoid a warning with asserts turned off */
 		ARG_UNUSED(err);
 		__ASSERT(err == CY_SYSCLK_SUCCESS, "Invalid clock selection");
+#if defined(CY_IP_M0S8WCO)
 		Cy_SysClk_ImoLock(CY_SYSCLK_IMO_LOCK_NONE);
+#endif
 		SystemCoreClockUpdate();
 #endif
 		break;

yurii2001lozyn/zephyr_fork1

drivers/clock_control/clock_control_infineon_fixed_clock.c
@@ -172,7 +172,9 @@ static int fixed_rate_clk_init(const struct device *dev)
 		/* "touch" err to avoid a warning with asserts turned off */
 		ARG_UNUSED(err);
 		__ASSERT(err == CY_SYSCLK_SUCCESS, "Invalid clock selection");
+#if defined(CONFIG_INFINEON_SYSCLK_LOCK_NONE)
 		Cy_SysClk_ImoLock(CY_SYSCLK_IMO_LOCK_NONE);
+#endif
 		SystemCoreClockUpdate();
 #endif
 		break;